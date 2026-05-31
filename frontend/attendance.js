87c6410f36ef60b2210a5a9d8834f4da50ccce04
ROSHINI <kakinadaroshini@gmail.com>
2026-05-31 19:28:08 +0530

    Updated project

--- frontend/attendance.js
+++ frontend/attendance.js
@@ -1,6 +1,7 @@
 ﻿const employeeId = localStorage.getItem("employeeId");
 
 if (!employeeId) {
+
   window.location.href = "role.html";
 }
 
@@ -9,21 +10,32 @@ async function loadAttendance() {
   try {
 
     const response = await fetch(
+
       `http://localhost:5000/attendance/${employeeId}`
+
     );
 
     const data = await response.json();
 
-    const tbody = document.getElementById("attendanceTable");
+    const tbody =
+    document.getElementById(
+      "attendanceTable"
+    );
 
     if (!data.length) {
 
       tbody.innerHTML = `
+
         <tr>
-          <td colspan="2">
+
+          <td colspan="4">
+
             No attendance records available
+
           </td>
+
         </tr>
+
       `;
 
       return;
@@ -32,34 +44,61 @@ async function loadAttendance() {
     tbody.innerHTML = data.map((item) => {
 
       const formattedDate =
-        new Date(item.attendance_date)
-        .toLocaleDateString("en-GB");
+      new Date(item.attendance_date)
+      .toLocaleDateString(
+        "en-GB"
+      );
 
       return `
+
         <tr>
-          <td>${formattedDate}</td>
-          <td>${item.status}</td>
+
+          <td>
+            ${item.employee_id}
+          </td>
+
+          <td>
+            ${formattedDate}
+          </td>
+
+          <td>
+            ${item.day}
+          </td>
+
+          <td class="
+            ${item.status.toLowerCase()}
+          ">
+
+            ${item.status}
+
+          </td>
+
         </tr>
+
       `;
 
     }).join("");
 
   }
 
-  catch (error) {
+  catch(error){
 
-    document.getElementById("attendanceTable").innerHTML = `
+    document.getElementById(
+      "attendanceTable"
+    ).innerHTML = `
+
       <tr>
-        <td colspan="2">
+
+        <td colspan="4">
+
           Unable to load attendance
+
         </td>
+
       </tr>
+
     `;
-
-    console.log(error);
-
   }
-
 }
 
 loadAttendance();
